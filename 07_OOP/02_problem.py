2090e6905928b93d6cf7197b643c9725e041df1b
shahnawazkakarh <shahnawaz.jrw@gmail.com>
2025-08-18 02:00:01 +0500

    oop problem solving

--- 07_OOP/02_problem.py
+++ 07_OOP/02_problem.py
@@ -1,3 +1,6 @@
+# Class Method and Self
+# Problem: Add a method to the Car class that displays the full name of the car (brand and model).
+
 # Basic Class and Object
 # Problem: Create a Car class with attributes/variables like brand and model.
 # Then create an instance of this class.
@@ -14,6 +17,9 @@ class Car:
         # Assign the parameter 'model' to the object's 'model' property
         self.model = model
 
+    def full_name(self):
+        return f"{self.brand} {self.model}"
+
 # Creating an instance (object) of Car class
 # Here, 'my_car' becomes a Car object with brand="Toyota", model="Corolla"
 my_car = Car("Toyota", "Corolla")
@@ -21,11 +27,12 @@ my_car = Car("Toyota", "Corolla")
 # Accessing the object's properties using dot notation
 print(my_car.brand)   # Output: Toyota
 print(my_car.model)   # Output: Corolla
+print(my_car.full_name())    # Output: Toyota Corolla
 
 
 # Reassigning 'my_car' to a new Car object with different values
 # This creates a new Car object (brand="Honda", model="Civic")
-my_car = Car("Honda", "Civic")
+my_car2 = Car("Honda", "Civic")
 
 # Now 'my_car' points to this new object, not the previous Toyota one
-print(my_car.model)   # Output: Civic
+print(my_car2.brand)   # Output: Civic
