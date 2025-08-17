8a1ef93d0b3987c84a8a304896104ef4c78c6f75
shahnawazkakarh <shahnawaz.jrw@gmail.com>
2025-08-17 19:04:18 +0500

    yield function problem 09 solved

--- 05_functions/09_problem.py
+++ 05_functions/09_problem.py
@@ -1,2 +1,14 @@
 # Generator Function with yield
 # Problem: Write a generator function that yields even numbers up to a specified limit.
+
+def even_generater(limit):
+    for i in range(2, limit + 1, 2):
+        yield i # puts the function along with its state in memory, (like how much work is being performed internally)
+
+
+for num in even_generater(10):
+    print(num)
+
+print(list(even_generater(10)))  # This will print: [2, 4, 6, 8, 10]
+
+###############################################################
